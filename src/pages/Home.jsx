60a8f7e8a6b71f29ffae1474515280e3862368a9
PabloTei <pteijeirolopez@gmail.com>
2023-02-14 21:49:15 +0100

    css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,13 @@ const Home = () => {
     return (
         <div className="home">
             <h1>¡BIENVENIDX {localStorage.getItem("user")}!</h1>
+            <div className="links">
             <NavLink to="monsters">Monsters</NavLink>
             <NavLink to="equipment">Equipment</NavLink>
+            </div>
+            <div className="logout">
             <Logout/>
+            </div>
         </div>
     )
 }
